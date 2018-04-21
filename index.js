7e366698a0181bb2880d10a401e0ac0a9a967fcb
Chris Henry <cdhenry@gmail.com>
2018-04-21 23:21:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const returnFirstTwoDrivers = function (drivers) {return drivers.slice(0,2)};
 const returnLastTwoDrivers = function (drivers) {return drivers.slice(-2)};
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (integer) {
+  return function(fare){return (fare * integer)};
+}
